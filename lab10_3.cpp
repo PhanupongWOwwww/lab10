1971b2edb31429518ed83b378214917a6262da3f
PhanupongWOwwww <Zozackgt@gmail.com>
2025-12-26 07:48:19 +0700

    lab10_3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream file("score.txt");
+
+    double x, sum = 0, sumSq = 0;
+    int count = 0;
+
+    while(file >> x){
+        sum += x;
+        sumSq += x * x;
+        count++;
+    }
+
+    file.close();
+
+    double mean = sum / count;
+    double sd = sqrt((sumSq / count) - (mean * mean));
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd << endl;
+
+    return 0;
+}
